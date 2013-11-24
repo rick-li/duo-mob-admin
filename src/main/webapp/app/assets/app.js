6f36334871fbd87208baa8bba466cdfba5885892
Rick Li <racke1983cn@gmail.com>
2013-11-24 23:37:40 +0800

    replace backend with parse, and finished the form

--- src/main/webapp/app/assets/app.js
+++ src/main/webapp/app/assets/app.js
@@ -249,36 +249,116 @@ true;return this};m.prototype.value=function(){return this._wrapped}}).call(this
 
 app.constant('TPL_PATH', 'templates')
 
-// app.config(function($routeProvider, TPL_PATH) {
-//   $routeProvider.when('/',{
-//     controller : 'HomeCtrl',
-//     templateUrl : TPL_PATH + '/home.html'
-//   });
-// });
-app.constant('AK', 'jX5URdvxVdL2Y4ZuvXKRdABw')
-app.constant('frontia', baidu.frontia)
-app.service('FrontiaService', function(frontia) {
-   var AK = "jX5URdvxVdL2Y4ZuvXKRdABw"
-  frontia.init(AK);
-  return function () {
-    
-  }
-});
+
+app.constant('Parse', Parse);
+
 app.controller('MenuCtrl', function($scope, $log) {
-  $log.log('menu')
+    $log.log('menu')
 });
-app.controller('UserCtrl', function($scope, $log) {
+app.controller('UserCtrl', function($scope, $log, Parse) {
+    $log.log('user ctrl')
+    $scope.currentUser = Parse.User.current();
+
+
+    $log.log($scope.currentUser);
+});
+
+app.controller('LoginCtrl', function($scope, $log, Parse) {
+    $scope.login = function() {
+        $log.log('name: ' + $scope.username + " password: " + $scope.password);
+        var user = Parse.User.logIn($scope.username, $scope.password, {
+            success: function(user) {
+                $log.log('login success');
+                $log.log(user);
+
+                $scope.currentUser = user;
+
+                $scope.$apply();
+            }
+        });
+    }
+
+});
+app.controller('CategoryCtrl', function($scope, $rootScope, $log, Parse) {
+    $log.log('CategoryCtrl')
+    var Category = Parse.Object.extend("Category");
+
+    var query = new Parse.Query(Category);
+    query.descending("updatedAt");
+
+    query.find().then(function(results) {
+            $log.log('categories')
+            $log.log(results);
+            $scope.categories = results;
+            $scope.activeCategory = $scope.categories[0];
+            $rootScope.$emit('categoryChg', $scope.activeCategory)
+            $log.log($scope.activeCategory.id);
+            $scope.$apply();
+        }
+    )
+
+})
+app.controller('ContentCtrl', function($scope, $rootScope, $log, Parse) {
+    $log.log('ContentCtrl')
+    var Article = Parse.Object.extend("Article");
+
+    var query = new Parse.Query(Article);
+    query.descending("updatedAt");
+
+    $rootScope.$on('categoryChg', function(e, category) {
+        $log.log('category is ' + category);
+        query.find().then(function(results) {
+                $log.log('articles')
+                $log.log(results);
+                $scope.articles = results;
+                $scope.$apply();
+            }
+        )
+    });
+
+    $scope.viewDetail = function(item) {
+      $log.log('view detail');
+      $rootScope.$emit('viewDetail', item);
+    };
+});
+
+app.controller('EditCtrl', function($scope, $rootScope, $log, Parse) {
+  $log.log('EditCtrl');
   
-  $scope.user = window.user
-  $log.log('user '+$scope.user.accountName);
-});
+  $scope.fileChanged = function(fileEl) {
+    $log.log(fileEl);
+    $scope.changedFileEl = fileEl;
+  };
+  
+  $scope.save = function() {
+    $log.log('save')
+    if($scope.changedFileEl && $scope.changedFileEl.files.length>0){
+      var fileRef = $scope.changedFileEl.files[0]
+      $log.log(fileRef);
+      var parseFile = new Parse.File(fileRef.name, fileRef);
+      parseFile.save().then(function() {
+        $log.log('file saved '+fileRef.name)
 
-app.controller('ContentCtrl', function($scope, $log, AK, frontia) {
-    $log.log('content ctrl ' + AK);
+        $scope.activeDetailItem.set('image', parseFile);
+        $scope.activeDetailItem.save().then(function () {
+          $log.log('saved success with image');
+        });
+      });
+      
+    }else{
+      $scope.activeDetailItem.save().then(function () {
+        $log.log('saved success without');
+      });
+    }
 
-    
+  };
+  $scope.closeme = function() {
+    $log.log('close')
+    $scope.activeDetailItem = null;
 
+  };
+  $rootScope.$on('viewDetail', function(e, item) {
 
-
-
+    $scope.activeDetailItem = item;
+  });
 });
